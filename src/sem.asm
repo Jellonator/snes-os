d895202706fa0b802b23f92ff0863e65d59901f0
Jocelyn Beedie <jellonator00@gmail.com>
2022-07-23 04:23:37 -0400

    initial semaphore implementation

--- src/sem.asm
+++ src/sem.asm
@@ -6,34 +6,88 @@
 ; create a semaphore, returns SID8 in X
 ; initializes semaphore to A
 semcreate:
-;     sep #$30 ; 8b AXY
-;     pha
-;     .DisableInt__
-;     ldx #0
-;     -:
-;     ; TODO: fail if semtab is full
-;     inx ; start at 1
-;     lda.l kSemPIDTable,X
-;     bpl - ; if top bit is 0, try again
-;     lda #0
-;     sta.l kSemCountTable,X
-;     .RestoreInt__
-;     pla
-;     sta.l kSemCountTable,X
+    sep #$30 ; 8b AXY
+    pha
+    .DisableInt__
+    ldx #KQID_SEM-1 ; SID8 is actually synonymous with QID8, for efficiency sake
+    ; TODO: fail if semtab is full
+    - inx ; start at 0
+    lda.l kQueueTabNext,X
+    bne - ; try next if not null
+    .RestoreInt__
+    ; clear queue
+    txa
+    sta.l kQueueTabNext,X ; X->next = X
+    sta.l kQueueTabPrev,X ; X->prev = X
+    ; set count
+    pla
+    sta.l kSemTabCount-KQID_SEM,X
     rtl
 
+; wait for semaphore in X
 semwait:
+    sep #$30
+    phb
+    .ChangeDataBank $7E
+    .DisableInt__
+; begin
+    dec.w loword(kSemTabCount-KQID_SEM),X
+    bpl +
+    ; --count < 0, block
+        ; insert PID into queue
+        lda.w loword(kCurrentPID)
+        tay
+        jsl kenqueue
+        ; suspend
+        tyx
+        jsl process_suspend
+        .RestoreInt__
+        jsl kreschedule
+        ; resume
+        plb
+        rtl
+    +:
+; end
+    sep #$30
+    .RestoreInt__
+    plb
     rtl
 
 semsignal:
+    sep #$30
+    phb
+    .ChangeDataBank $7E
+    .DisableInt__
+; begin
+    lda.w loword(kSemTabCount-KQID_SEM),X
+    bcs +
+        jsl kdequeue
+        tyx
+        jsl kresumeprocess
+    +:
+    inc.w loword(kSemTabCount-KQID_SEM),X
+; kSemTabCount
+    sep #$30
+    .RestoreInt__
+    plb
+    rtl
     rtl
 
-; ; Frees the semephore in X
+; Frees the semaphore in X
 semdelete:
-;     sep #$20
-;     lda #80
-;     sta.l kSemPIDTable,X
-;     ; TODO: free waiting processes
+    sep #$30
+; kill waiting processes
+@looprm:
+    jsl kdequeue
+    cpy #0
+    beq @endrm
+    phx
+    tyx
+    jsl kkill
+    plx
+    bra @looprm
+@endrm:
+    jsl kdelqueue
     rtl
 
 .ENDS
