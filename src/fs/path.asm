37e590c5c6ef1db349e84ceb5b423e8db2a136bf
Jocelyn Beedie <jellonator00@gmail.com>
2024-09-29 13:49:26 -0400

    Added path handling functions

---
+++ src/fs/path.asm
@@ -0,0 +1,230 @@
+.include "base.inc"
+
+.BANK $01 SLOT "ROM"
+.SECTION "Path" FREE
+
+; Return true if string in X is an absolute path
+path_is_absolute:
+    .INDEX 16
+    sep #$20
+    lda.w $0000,X
+    cmp #'/'
+    beq +
+        lda #0
+        rtl ; False - first char is '/'
+    +:
+    lda #1
+    rtl ; True - first char is not '/'
+
+; Return true if string in X is a relative path
+path_is_relative:
+    .INDEX 16
+    sep #$20
+    lda.w $0000,X
+    cmp #'/'
+    beq +
+        lda #1
+        rtl ; True - first char is '/'
+    +:
+    lda #0
+    rtl ; False - first char is not '/'
+
+; Return true if string in X is empty
+path_is_empty:
+    .INDEX 16
+    sep #$20
+    lda.w $0000,X
+    beq +
+        lda #0
+        rtl ; False - first char not null
+    +:
+    lda #1
+    rtl ; True - first char is null
+
+; Get pointer to tail of X
+path_get_tail_ptr:
+    .INDEX 16
+    sep #$20
+    lda.w $0000,X
+    cmp #0
+    beq @end_no_inx ; already reached null, just end
+    cmp #'/'
+    bne +
+        inx ; skip first '/'
+    +:
+    ; skip until null or /
+@loop:
+    lda.w $0000,X
+    cmp #'/'
+    beq @end_with_inx
+    cmp #0
+    beq @end_no_inx
+    inx
+    jmp @loop
+@end_with_inx:
+    inx
+@end_no_inx:
+    rtl
+
+; Validates path in X
+; Returns with A=0 if invalid, or A=1 if valid
+path_validate:
+    .INDEX 16
+    sep #$20
+    phx ; [+2; 2]
+    lda.w $0000,X
+    cmp #0
+    bne +
+        ; empty string, return 0
+        lda #0
+        plx
+        rtl
+    +:
+    cmp #'/'
+    bne +
+        inx
+    +:
+    ldy #0
+    ; check up to 'FS_MAX_FILENAME_LEN' characters
+@loop:
+    lda.w $0000,X
+    ; cmp #0 (commented out - not necessary after lda)
+    bne +
+        ; reached null; success
+        lda #1
+        plx
+        rtl
+    +:
+    cmp #'/'
+    bne +
+        ; reached /, fail if Y=0
+        cpy #0
+        bne ++
+            lda #0
+            plx
+            rtl
+        ++:
+        ; otherwise, parse next block
+        inx
+        ldy #0
+        jmp @loop
+    +:
+    ; found a character
+    cpy #FS_MAX_FILENAME_LEN
+    bne +
+        ; too many characters in part, fail
+        lda #0
+        plx
+        rtl
+    +:
+    iny
+    inx
+    jmp @loop
+
+; Compare two paths pieces in separate buffers
+; Push order:
+; src  [dl], $07
+; dest [dl], $04
+; This is similar to `stringCmpL`, except:
+;  * we only check up to 14 characters
+;  * '/' is treated as a null
+path_compare_pieces:
+    rep #$30
+    tsc ; A = SP
+    phd ; Push DP
+    tcd ; DP = previous SP
+    sep #$20 ; 8B A, 16B XY
+    ldy #0 ; Y = 0
+    ldx #FS_MAX_FILENAME_LEN ; X = LEN
+    bra @loop
+@continue:
+    iny
+    dex
+    bne +
+        ; reached max path piece length, must be null
+        pld
+        lda.b #0
+        rtl
+    +:
+@loop:
+    lda.b [$07],Y
+    beq @xnull
+    cmp #'/'
+    beq @xnull
+    cmp.b [$04],Y
+    beq @continue
+    ; very slight difference here;
+    ; "abc." < "abc/", but "abc." > "abc"
+    bcc +
+        pld
+        lda.b #$01
+        rtl
+    +:
+        pld
+        lda.b #$FF
+        rtl
+@xnull:
+    lda.b [$04],Y
+    bne +
+        pld
+        rtl
+    +:
+    cmp #'/'
+    bne +
+        lda #0
+        pld
+        rtl
+    +:
+    lda.b #$FF
+    pld
+    rtl
+
+; Reads next piece from path
+; Push order:
+; src  [dl], $07
+; dest [dl], $04
+; `dest` should be at least FS_MAX_FILENAME_LEN+1 bytes
+; `src` will be incremented to the end of the piece
+; `dest` will be incremented to the end of the string (nullptr)
+path_split_into_buffer:
+    rep #$30
+    tsc ; A = SP
+    phd ; Push DP
+    tcd ; DP = previous SP
+    sep #$20 ; 8B A, 16B XY
+    ; if *src == '/':
+    lda.b [$07]
+    cmp #'/'
+    bne +
+        ; ++ src
+        inc.b $07
+        lda.b [$07]
+    +:
+    ldx #FS_MAX_FILENAME_LEN
+    ; do {
+@loop:
+    ; if (*src == '/' || *src == '\0') goto @finish
+    cmp #'/'
+    beq @finish
+    cmp #0
+    beq @finish
+    ; *dest = *src
+    sta.b [$04]
+    ; ++ dest
+    inc.b $04
+    ; ++ src
+    inc.b $07
+    ; } while (--x != 0)
+    dex
+    beq @finish
+    lda.b [$07]
+    jmp @loop
+@finish:
+    ; *dest = '\0'
+    lda #0
+    sta.b [$04]
+    ; return
+    pld
+    rtl
+
+.ENDS
--- src/test/test.asm
+++ src/test/test.asm
@@ -45,6 +45,53 @@ _testnum_N32768:
 _teststrempty:
     .db "\0"
 
+_test_invalid_path1:
+    .db "\0"
+
+_test_invalid_path2:
+    .db "/home/foo//bar\0"
+
+_test_invalid_path3:
+    .db "/home/abcdefghijklmnop/foo\0"
+
+_test_valid_path1:
+    .db "/home/foo/bar\0"
+
+_test_valid_path1_piece1:
+    .db "home\0"
+
+_test_valid_path1_piece2:
+    .db "foo\0"
+
+_test_valid_path1_piece3:
+    .db "bar\0"
+
+_test_valid_path2:
+    .db "/\0"
+
+_test_valid_path3:
+    .db "/static/data/"
+
+_test_valid_path3_piece1:
+    .db "static\0"
+
+_test_valid_path3_piece2:
+    .db "data\0"
+
+_test_valid_path4:
+    .db "abcdefghijklmn/opqrstuvwxyz12\0"
+
+_test_valid_path4_piece1:
+    .db "abcdefghijklmn"
+; no NULL at end needed
+
+_test_valid_path4_piece2:
+    .db "opqrstuvwxyz12"
+; no NULL at end needed
+
+_test_valid_path5:
+    .db ".\0"
+
 .MACRO .StartGroup ARGS groupname
     rep #$10
     ldy #@@@@@\.\@n
@@ -117,363 +164,508 @@ shTest
     .ChangeDataBank $01
     ; TEST memoryCmp8
     .StartGroup "memoryCmp8"
-    rep #$10 ; 16b XY
-    sep #$20 ; 8b A
-    lda #4
-    ldx #_teststr1
-    ldy #_teststr2
-    jsl memoryCmp8
-    .CheckAEq 0
-    rep #$10 ; 16b XY
-    sep #$20 ; 8b A
-    lda #5
-    ldx #_teststr1
-    ldy #_teststr2
-    jsl memoryCmp8
-    .CheckANeq 0
-    rep #$10 ; 16b XY
-    sep #$20 ; 8b A
-    lda #0
-    ldx #_teststr1
-    ldy #_teststr3
-    jsl memoryCmp8
-    .CheckAEq 0
-    rep #$10 ; 16b XY
-    sep #$20 ; 8b A
-    lda #1
-    ldx #_teststr1
-    ldy #_teststr3
-    jsl memoryCmp8
-    .CheckANeq 0
+        rep #$10 ; 16b XY
+        sep #$20 ; 8b A
+        lda #4
+        ldx #_teststr1
+        ldy #_teststr2
+        jsl memoryCmp8
+        .CheckAEq 0
+        rep #$10 ; 16b XY
+        sep #$20 ; 8b A
+        lda #5
+        ldx #_teststr1
+        ldy #_teststr2
+        jsl memoryCmp8
+        .CheckANeq 0
+        rep #$10 ; 16b XY
+        sep #$20 ; 8b A
+        lda #0
+        ldx #_teststr1
+        ldy #_teststr3
+        jsl memoryCmp8
+        .CheckAEq 0
+        rep #$10 ; 16b XY
+        sep #$20 ; 8b A
+        lda #1
+        ldx #_teststr1
+        ldy #_teststr3
+        jsl memoryCmp8
+        .CheckANeq 0
     .EndGroup
     ; TEST stringCmp
     .StartGroup "stringCmp"
-    rep #$10 ; 16b XY
-    ldx #_teststr1
-    ldy #_teststr2
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq -1
-    rep #$10 ; 16b XY
-    ldx #_teststr2
-    ldy #_teststr1
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 1
-    rep #$10 ; 16b XY
-    ldx #_teststr2
-    ldy #_teststr22
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq -1
-    rep #$10 ; 16b XY
-    ldx #_teststr22
-    ldy #_teststr2
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 1
-    rep #$10 ; 16b XY
-    ldx #_teststrempty
-    ldy #_teststr2
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq -1
-    rep #$10 ; 16b XY
-    ldx #_teststr2
-    ldy #_teststrempty
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 1
-    rep #$10 ; 16b XY
-    ldx #_teststrempty
-    ldy #_teststrempty
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$10 ; 16b XY
-    ldx #_teststr3
-    ldy #_teststr3
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
+        rep #$10 ; 16b XY
+        ldx #_teststr1
+        ldy #_teststr2
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq -1
+        rep #$10 ; 16b XY
+        ldx #_teststr2
+        ldy #_teststr1
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 1
+        rep #$10 ; 16b XY
+        ldx #_teststr2
+        ldy #_teststr22
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq -1
+        rep #$10 ; 16b XY
+        ldx #_teststr22
+        ldy #_teststr2
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 1
+        rep #$10 ; 16b XY
+        ldx #_teststrempty
+        ldy #_teststr2
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq -1
+        rep #$10 ; 16b XY
+        ldx #_teststr2
+        ldy #_teststrempty
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 1
+        rep #$10 ; 16b XY
+        ldx #_teststrempty
+        ldy #_teststrempty
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$10 ; 16b XY
+        ldx #_teststr3
+        ldy #_teststr3
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
     .EndGroup
     ; TEST stringCmpL
     .StartGroup "stringCmpL"
-    .PEAL _teststr1
-    .PEAL _teststr2
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq -1
-    .POPN 6
-    .PEAL _teststr2
-    .PEAL _teststr1
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq 1
-    .POPN 6
-    .PEAL _teststr2
-    .PEAL _teststr22
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq -1
-    .POPN 6
-    .PEAL _teststr22
-    .PEAL _teststr2
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq 1
-    .POPN 6
-    .PEAL _teststrempty
-    .PEAL _teststr2
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq -1
-    .POPN 6
-    .PEAL _teststr2
-    .PEAL _teststrempty
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq 1
-    .POPN 6
-    .PEAL _teststrempty
-    .PEAL _teststrempty
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq 0
-    .POPN 6
-    .PEAL _teststr3
-    .PEAL _teststr3
-    jsl stringCmpL
-    .ACCU 8
-    .CheckAEq 0
-    .POPN 6
+        .PEAL _teststr1
+        .PEAL _teststr2
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq -1
+        .POPN 6
+        .PEAL _teststr2
+        .PEAL _teststr1
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq 1
+        .POPN 6
+        .PEAL _teststr2
+        .PEAL _teststr22
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq -1
+        .POPN 6
+        .PEAL _teststr22
+        .PEAL _teststr2
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq 1
+        .POPN 6
+        .PEAL _teststrempty
+        .PEAL _teststr2
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq -1
+        .POPN 6
+        .PEAL _teststr2
+        .PEAL _teststrempty
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq 1
+        .POPN 6
+        .PEAL _teststrempty
+        .PEAL _teststrempty
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq 0
+        .POPN 6
+        .PEAL _teststr3
+        .PEAL _teststr3
+        jsl stringCmpL
+        .ACCU 8
+        .CheckAEq 0
+        .POPN 6
     .EndGroup
     ; TEST stringFindChar
     .StartGroup "stringFindChar"
-    rep #$10 ; 16b XY
-    sep #$20 ; 8b A
-    lda #'b'
-    ldx #_teststr4
-    jsl stringFindChar
-    pha
-    phx
-    php
-    .CheckXEq _teststr4
-    plp
-    plx
-    pla
-    inx
-    jsl stringFindChar
-    pha
-    phx
-    php
-    .CheckXEq _teststr4+4
-    plp
-    plx
-    pla
-    inx
-    jsl stringFindChar
-    pha
-    phx
-    php
-    .CheckXEq _teststr4+7
-    plp
-    plx
-    pla
-    inx
-    jsl stringFindChar
-    pha
-    phx
-    php
-    .CheckXEq 0
-    plp
-    plx
-    pla
-    inx
+        rep #$10 ; 16b XY
+        sep #$20 ; 8b A
+        lda #'b'
+        ldx #_teststr4
+        jsl stringFindChar
+        pha
+        phx
+        php
+        .CheckXEq _teststr4
+        plp
+        plx
+        pla
+        inx
+        jsl stringFindChar
+        pha
+        phx
+        php
+        .CheckXEq _teststr4+4
+        plp
+        plx
+        pla
+        inx
+        jsl stringFindChar
+        pha
+        phx
+        php
+        .CheckXEq _teststr4+7
+        plp
+        plx
+        pla
+        inx
+        jsl stringFindChar
+        pha
+        phx
+        php
+        .CheckXEq 0
+        plp
+        plx
+        pla
+        inx
     .EndGroup
     ; TEST stringLen
     .StartGroup "stringLen"
-    rep #$10
-    ldx #_teststrempty
-    jsl stringLen
-    .ACCU 16
-    .CheckAEq 0
-    rep #$10
-    ldx #_teststr1
-    jsl stringLen
-    .ACCU 16
-    .CheckAEq 7
-    rep #$10
-    ldx #_teststr22
-    jsl stringLen
-    .ACCU 16
-    .CheckAEq 8
+        rep #$10
+        ldx #_teststrempty
+        jsl stringLen
+        .ACCU 16
+        .CheckAEq 0
+        rep #$10
+        ldx #_teststr1
+        jsl stringLen
+        .ACCU 16
+        .CheckAEq 7
+        rep #$10
+        ldx #_teststr22
+        jsl stringLen
+        .ACCU 16
+        .CheckAEq 8
     .EndGroup
     ; TEST stringToU16
     .StartGroup "stringToU16"
-    rep #$10
-    ldx #_testnum_420
-    jsl stringToU16
-    .ACCU 16
-    .CheckAEq 420
-    rep #$10
-    ldx #_testnum_32767
-    jsl stringToU16
-    .ACCU 16
-    .CheckAEq 32767
-    rep #$10
-    ldx #_testnum_65535
-    jsl stringToU16
-    .ACCU 16
-    .CheckAEq 65535
-    rep #$10
-    ldx #_testnum_0
-    jsl stringToU16
-    .ACCU 16
-    .CheckAEq 0
+        rep #$10
+        ldx #_testnum_420
+        jsl stringToU16
+        .ACCU 16
+        .CheckAEq 420
+        rep #$10
+        ldx #_testnum_32767
+        jsl stringToU16
+        .ACCU 16
+        .CheckAEq 32767
+        rep #$10
+        ldx #_testnum_65535
+        jsl stringToU16
+        .ACCU 16
+        .CheckAEq 65535
+        rep #$10
+        ldx #_testnum_0
+        jsl stringToU16
+        .ACCU 16
+        .CheckAEq 0
     .EndGroup
     ; TEST stringToI16
     .StartGroup "stringToI16"
-    rep #$10
-    ldx #_testnum_420
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq 420
-    rep #$10
-    ldx #_testnum_32767
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq 32767
-    rep #$10
-    ldx #_testnum_0
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq 0
-    rep #$10
-    ldx #_testnum_N0
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq 0
-    rep #$10
-    ldx #_testnum_N420
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq -420
-    rep #$10
-    ldx #_testnum_N32767
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq -32767
-    rep #$10
-    ldx #_testnum_N32768
-    jsl stringToI16
-    .ACCU 16
-    .CheckAEq -32768
+        rep #$10
+        ldx #_testnum_420
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq 420
+        rep #$10
+        ldx #_testnum_32767
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq 32767
+        rep #$10
+        ldx #_testnum_0
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq 0
+        rep #$10
+        ldx #_testnum_N0
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq 0
+        rep #$10
+        ldx #_testnum_N420
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq -420
+        rep #$10
+        ldx #_testnum_N32767
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq -32767
+        rep #$10
+        ldx #_testnum_N32768
+        jsl stringToI16
+        .ACCU 16
+        .CheckAEq -32768
     .EndGroup
     ; TEST writeU16
     .StartGroup "writeU16"
-    rep #$30 ; 16b AXY
-    tdc
-    clc
-    adc #$08
-    sta.b $06 ; $06 = string buf
-    tax ; X = D + 8
-    lda #420
-    jsl writeU16
-    ldx.b $06
-    ldy.w #_testnum_420
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #0
-    jsl writeU16
-    ldx.b $06
-    ldy.w #_testnum_0
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #32767
-    jsl writeU16
-    ldx.b $06
-    ldy.w #_testnum_32767
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #65535
-    jsl writeU16
-    ldx.b $06
-    ldy.w #_testnum_65535
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
+        rep #$30 ; 16b AXY
+        tdc
+        clc
+        adc #$08
+        sta.b $06 ; $06 = string buf
+        tax ; X = D + 8
+        lda #420
+        jsl writeU16
+        ldx.b $06
+        ldy.w #_testnum_420
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #0
+        jsl writeU16
+        ldx.b $06
+        ldy.w #_testnum_0
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #32767
+        jsl writeU16
+        ldx.b $06
+        ldy.w #_testnum_32767
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #65535
+        jsl writeU16
+        ldx.b $06
+        ldy.w #_testnum_65535
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
     .EndGroup
     ; TEST writeI16
     .StartGroup "writeI16"
-    rep #$30 ; 16b AXY
-    tdc
-    clc
-    adc #$08
-    sta.b $06 ; $06 = string buf
-    tax ; X = D + 8
-    lda #420
-    jsl writeI16
-    ldx.b $06
-    ldy.w #_testnum_420
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #0
-    jsl writeI16
-    ldx.b $06
-    ldy.w #_testnum_0
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #32767
-    jsl writeI16
-    ldx.b $06
-    ldy.w #_testnum_32767
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #-32767
-    jsl writeI16
-    ldx.b $06
-    ldy.w #_testnum_N32767
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #-32768
-    jsl writeI16
-    ldx.b $06
-    ldy.w #_testnum_N32768
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
-    rep #$30 ; 16b AXY
-    ldx.b $06
-    lda #-420
-    jsl writeI16
-    ldx.b $06
-    ldy.w #_testnum_N420
-    jsl stringCmp
-    .ACCU 8
-    .CheckAEq 0
+        rep #$30 ; 16b AXY
+        tdc
+        clc
+        adc #$08
+        sta.b $06 ; $06 = string buf
+        tax ; X = D + 8
+        lda #420
+        jsl writeI16
+        ldx.b $06
+        ldy.w #_testnum_420
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #0
+        jsl writeI16
+        ldx.b $06
+        ldy.w #_testnum_0
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #32767
+        jsl writeI16
+        ldx.b $06
+        ldy.w #_testnum_32767
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #-32767
+        jsl writeI16
+        ldx.b $06
+        ldy.w #_testnum_N32767
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #-32768
+        jsl writeI16
+        ldx.b $06
+        ldy.w #_testnum_N32768
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
+        rep #$30 ; 16b AXY
+        ldx.b $06
+        lda #-420
+        jsl writeI16
+        ldx.b $06
+        ldy.w #_testnum_N420
+        jsl stringCmp
+        .ACCU 8
+        .CheckAEq 0
     .EndGroup
+    .StartGroup "PathCheck"
+        rep #$10
+        ldx #loword(_test_invalid_path1)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 0
+        ldx #loword(_test_invalid_path2)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 0
+        ldx #loword(_test_invalid_path3)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 0
+        ldx #loword(_test_valid_path1)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 1
+        ldx #loword(_test_valid_path2)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 1
+        ldx #loword(_test_valid_path3)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 1
+        ldx #loword(_test_valid_path4)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 1
+        ldx #loword(_test_valid_path5)
+        jsl path_validate
+        .ACCU 8
+        .CheckAEq 1
+    .EndGroup
+    .StartGroup "PathCmp"
+        .PEAL _test_valid_path1_piece1
+        .PEAL _test_valid_path1_piece1
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 6
+        .PEAL _test_valid_path1_piece1
+        .PEAL _test_valid_path1_piece2
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckANeq 0
+        .POPN 6
+        .PEAL _teststrempty
+        .PEAL _test_valid_path1_piece2
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckANeq 0
+        .POPN 6
+        .PEAL _test_valid_path1_piece2
+        .PEAL _teststrempty
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckANeq 0
+        .POPN 6
+        .PEAL _test_valid_path3+1
+        .PEAL _test_valid_path3_piece1
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 6
+        .PEAL _test_valid_path3+8
+        .PEAL _test_valid_path3_piece2
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 6
+        .PEAL _test_valid_path4
+        .PEAL _test_valid_path4_piece1
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 6
+        .PEAL _test_valid_path4+15
+        .PEAL _test_valid_path4_piece2
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 6
+        .PEAL _test_valid_path5
+        .PEAL _test_valid_path5
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 6
+    .EndGroup
+    .StartGroup "PathRead"
+        .PEAL _test_valid_path1
+        .PEAL kTempBuffer
+        jsl path_split_into_buffer
+        rep #$30
+        lda #loword(kTempBuffer)
+        sta $01,S
+        .PEAL _test_valid_path1_piece1
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 3
+        jsl path_split_into_buffer
+        rep #$30
+        lda #loword(kTempBuffer)
+        sta $01,S
+        .PEAL _test_valid_path1_piece2
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 3
+        jsl path_split_into_buffer
+        rep #$30
+        lda #loword(kTempBuffer)
+        sta $01,S
+        .PEAL _test_valid_path1_piece3
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 3
+        jsl path_split_into_buffer
+        rep #$30
+        lda #loword(kTempBuffer)
+        sta $01,S
+        .PEAL _teststrempty
+        jsl path_compare_pieces
+        .ACCU 8
+        .INDEX 16
+        .CheckAEq 0
+        .POPN 9
+    .EndGroup
+
     jsl procExit
 
 _errtxt: .db "ERROR\0"
